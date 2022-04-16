5268f9f20f6ca10b54dca8b74e8c80a23e2a8a3e
Miguel <chanampamiguel@gmail.com>
2022-04-15 23:42:09 -0300

    Punto 4

--- tp3_1.c
+++ tp3_1.c
@@ -3,25 +3,35 @@
 #include <string.h>
 
 int main() {
-    char *V[5];
+
     char *Buff;
+    char **listadoNombres;
+    int cantidadNombres;
+
+    // Ingresar la cantidad de nombres
+    printf("Ingrese la cantidad de nombres a ingresar: ");
+    scanf("%d", &cantidadNombres);
+    fflush(stdin);
+
+    listadoNombres = (char **) malloc(sizeof(char) * cantidadNombres);
 
     // Ingresar nombres
-    for (int i = 0; i < 5; i++) {
+    for (int i = 0; i < cantidadNombres; i++) {
+
         Buff = (char *) malloc(100 * sizeof(char));
         printf("Ingrese un nombre: ");
         gets(Buff);
-        V[i] = (char *) malloc((strlen(Buff)+1)*sizeof(char));
-        strcpy(V[i], Buff);
+        listadoNombres[i] = (char *) malloc((strlen(Buff)+1)*sizeof(char));
+        strcpy(listadoNombres[i], Buff);
     };
 
     // Mostrar nombres
     printf("Los nombres ingresados son:\n");
-    for (int i = 0; i < 5; i++) {
-        puts(V[i]);
-        free(V[i]);
+    for (int i = 0; i < cantidadNombres; i++) {
+        puts(listadoNombres[i]);
     };
-       
+
+    free(listadoNombres);
     free(Buff);
 
     return 0;
